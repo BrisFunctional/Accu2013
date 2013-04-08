16ec8b73d10f23401e00a826449eb45121c08d27
Thom Leggett <thomas.leggett@hp.com>
2013-04-08 18:28:50 +0100

    Adds skeleton hof examples project.

---
+++ 3_Functional_idioms/03_higher_order_functions/project.clj
@@ -0,0 +1,3 @@
+(defproject higher-order-functions "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.5.0"]])
---
+++ 3_Functional_idioms/03_higher_order_functions/src/higher_order_functions/examples.clj
@@ -0,0 +1,9 @@
+(ns higher-order-functions.examples)
+
+;; higher order functions can take functions as an argument
+
+
+
+;; and return them as a result
+
+;; 
